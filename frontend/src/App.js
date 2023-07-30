cbaa81016a9e42a4c1f6738426ab85c8229c0d88
unknown <nolan@thewellsfamily.us>
2023-07-30 13:30:15 -0700

    updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+
+import Home from "./views/Home";
+import Menu from "./views/Menu";
+import Order from "./views/Order";
+import Login from "./views/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container className='fixed-top'>
+        <Nav>
+        <Nav.Item>
+            <Nav.Link href="/">
+              <Link className='homeLink' to="/" style={{textDecoration: 'none'}}>Home</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/">
+              <Link className='menuLink' to="/Menu" style={{textDecoration: 'none'}}>Menu</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/">
+              <Link className='orderLink' to="/Order" style={{textDecoration: 'none'}}>Order Now</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/">
+              <Link className='loginLink' to="/Login" style={{textDecoration: 'none'}}>Login in</Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Menu" element={<Menu/>}/>
+        <Route path="/Order" element={<Order/>}/>
+        <Route path="/Login" element={<Login/>}/>
+      </Routes>
+    </Router>
   );
 }
 
