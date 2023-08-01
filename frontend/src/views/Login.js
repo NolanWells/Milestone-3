93d5eb8a7f4a63c33e9e929595b0140cb6e6e641
Bailey Franklin <baileydfranklin@gmail.com>
2023-07-31 17:28:09 -0700

    Added some code to the Login Page

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -1,5 +1,26 @@
-export default function Login(){
+import Container from "react-bootstrap/esm/Container";
+import Card from "react-bootstrap/Card";
+import { Button, Nav } from "react-bootstrap";
+
+export default function Login() {
     return (
-        <h1>Login</h1>
+        <Container className='fixed-top'>
+            <h2 style={{ textAlign: "center" }}>Create an Account</h2>
+            <Card style={{ textAlign: "center" }}>
+                <label for='username'>Username:</label> <br />
+                <input type='text' id='username' placeholder="Username" required />
+
+                <br />
+                <br />
+
+                <label for="password">Password:</label> <br />
+                <input type="password" id="passwork" placeholder="Password" required />
+
+                <br />
+                <br />
+
+                <Button>Submit</Button>
+            </Card>
+        </Container >
     )
 }
