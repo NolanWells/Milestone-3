0c5ff752bcd979cd1e7bce94ae7b94a9d1fd3273
unknown <nolan@thewellsfamily.us>
2023-08-01 20:58:58 -0700

    updated

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,5 +1,32 @@
-export default function Home(){
+import { Carousel } from "react-bootstrap"
+import ExampleCarouselImage from '.../Picture/Burger1.jpg'
+
+
+
+export default function Home() {
     return (
-    <h1>Home</h1>
+        <Carousel>
+            <Carousel.Item>
+                <ExampleCarouselImage text="First slide" />
+                <Carousel.Caption>
+                    <h3>Burger Picture</h3>
+                    <p>something that makes my mouth water</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <ExampleCarouselImage text="Second slide" />
+                <Carousel.Caption>
+                    <h3>Burger Picture</h3>
+                    <p>something that makes my mouth water</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <ExampleCarouselImage text="Third slide" />
+                <Carousel.Caption>
+                    <h3>Burger Picture</h3>
+                    <p>something that makes my mouth water</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
