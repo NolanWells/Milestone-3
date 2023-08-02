e0507978e6d329f1a1c10acbf6a2513d90a7b240
unknown <nolan@thewellsfamily.us>
2023-08-01 19:42:50 -0700

    updated

--- frontend/src/views/Menu.js
+++ frontend/src/views/Menu.js
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom'
-import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
 
 import Salads from './Menu/Salads'
 import Burgers from './Menu/Burgers'
@@ -13,20 +13,18 @@ export default function Menu() {
         <Container style={{ display: 'grid', placeItems: 'center' }}>
             <h1>SALADS</h1>
             <h2>----------------------------------------------------</h2>
-            <Card style={{ border: '1px solid black', textAlign: 'center', maxWidth: '80%' }}>
-                <Salads />
-            </Card>
+            <Salads/>
 
 
-            <Card style={{ border: '1px solid black', textAlign: 'center', maxWidth: '80%' }}>
+            <Card style={{ border: '1px solid black', textAlign: 'center'}}>
                 <h1>BURGERS</h1>
             </Card>
 
-            <Card style={{ border: '1px solid black', textAlign: 'center', maxWidth: '80%' }}>
+            <Card style={{ border: '1px solid black', textAlign: 'center'}}>
                 <h1>SIDES</h1>
             </Card>
 
-            <Card style={{ border: '1px solid black', textAlign: 'center', maxWidth: '80%' }}>
+            <Card style={{ border: '1px solid black', textAlign: 'center'}}>
                 <h1>DESERTS</h1>
             </Card>
         </Container>
--- frontend/src/views/Menu/Salads.js
+++ frontend/src/views/Menu/Salads.js
@@ -1,8 +1,29 @@
-
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
 
 export default function Salads() {
 
     return (
-        <h1>hello</h1>
+        <Container>
+        <Card style={{ border: '1px solid black', textAlign: 'center' }}>
+            <p>House Salad</p>
+        </Card>
+
+        <Card style={{ border: '1px solid black', textAlign: 'center'}}>
+            <p>Avo-Cobb-O Salad</p>
+        </Card>
+
+        <Card style={{ border: '1px solid black', textAlign: 'center'}}>
+            <p>Cripsy Chicken Tender Salad</p>
+        </Card>
+
+        <Card style={{ border: '1px solid black', textAlign: 'center'}}>
+            <p>Simply Grilled Chicken Salad</p>
+        </Card>
+        <Card style={{ border: '1px solid black', textAlign: 'center'}}>
+            <p>Southwest Salad</p>
+        </Card>
+
+        </Container>
     )
 }
