7f2b7010f2b5f0165ca5f3d7a23ba677cd74403f
unknown <nolan@thewellsfamily.us>
2023-08-01 18:10:48 -0700

    bailey

--- frontend/src/views/Menu.js
+++ frontend/src/views/Menu.js
@@ -1,5 +1,33 @@
+import { BrowserRouter as  Link} from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container';
+
+import Salads from './Menu/Salads'
+import Burgers from './Menu/Burgers'
+import Dessert from './Menu/Dessert'
+import Sides from './Menu/Sides'
+
+
 export default function Menu(){
     return (
-        <h1>bailey is mean to me</h1>
+            <Container>
+                 <Nav defaultActiveKey="/" variant="tabs" fill>
+                    <Nav.Item href="/Salads">
+                        <Link to='/Salads'>Salads</Link>
+                    </Nav.Item>
+
+                    <Nav.Item eventKey={"Burgers"}>
+                        <Link to= "/Burgers">Burgers</Link>
+                    </Nav.Item>
+
+                    <Nav.Item eventKey={"Sides"}>
+                        <Link to= "/Sides">Sides</Link>
+                    </Nav.Item>
+
+                    <Nav.Item eventKey={"Dessert"}>
+                        <Link to= "/Dessert">Dessert</Link>
+                    </Nav.Item>
+                </Nav>
+            </Container>
     )
 }
