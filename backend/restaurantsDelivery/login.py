1a53a0e7d833c24630e0c259ec8b83a9d937946d
Jose Martinez <josemartinez@Joses-Air.lan>
2023-08-06 02:37:53 -0700

    fixing post request

--- backend/restaurantsDelivery/login.py
+++ backend/restaurantsDelivery/login.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request
-from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 bp = Blueprint('login', __name__, url_prefix="/login")
-CORS(bp, resources={r"/login/*": {"origins": "http://localhost:3000"}})
+CORS(bp, resources={"/login/": {"origins": "http://localhost:5000"}})
+
 
 bcrypt = Bcrypt()
 
@@ -14,7 +15,7 @@ def login():
     data = request.get_json()
     username = data.get('Username')
     password = data.get('Password')
-    
+    print(password)
     # Generate a hashed password with a random salt
 
     hashed_password = bcrypt.generate_password_hash('123',10)
