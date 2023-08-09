df6f08109e90bf219534983ad9781be95e7d0f92
Jose <josemanuel21302@gmail.com>
2023-08-08 20:40:37 -0700

    Committing changes to Salads.js

--- frontend/src/views/Menu/Salads.js
+++ frontend/src/views/Menu/Salads.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Card, Button, CardImg, Row, Col } from 'react-bootstrap';
 import saladsData from './food/saladsData';
 
