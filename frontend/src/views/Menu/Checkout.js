90baa16343153c294df4e0adea013989a5d6f44a
Jose <josemanuel21302@gmail.com>
2023-08-12 02:50:10 -0700

    added an Ternary Operator in checkout, when cart is empty  add a link that sends users to menu

--- frontend/src/views/Menu/Checkout.js
+++ frontend/src/views/Menu/Checkout.js
@@ -6,6 +6,7 @@ import Button from 'react-bootstrap/Button';
 
 import items from './food/items';
 import { Card, CardImg, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 // import CheckoutBag from './CheckoutBag';
 
@@ -35,44 +36,50 @@ export default function Checkout() {
 
             {/* <CheckoutBag /> */}
 
-            { bag }
-            <Card style={{ border: '1px solid black', textAlign: 'center' }}>
-                <h1 style={{ textAlign: 'center' }}>Your Order</h1>
+            { bag.length > 0 ? 
+                <Card style={{ border: '1px solid black', textAlign: 'center' }}>
+                    <h1 style={{ textAlign: 'center' }}>Your Order</h1>
 
-                <Row>
-                {bag.map((itemID) => {
-                    const item = items.find(item => item.id === itemID);
-                    return (
-                        <Col key={item.id} xs={12} md={3}>
-                            <Card className="p-2" style={{ height: '32em' }}>
-                                <CardImg
-                                    variant='top'
-                                    src={item.image}
-                                    alt={item.name}
-                                    style={{ width: '100%', height: '10em' }}
-                                />
-                                <Card.Text>
-                                    <b>{item.name}</b>
-                                </Card.Text>
-                                <hr />
-                                <div className="flex-grow-1" style={{ backgroundImage: 'linear-gradient(105deg, darkred, black)', color: 'white', borderRadius: '15%' }}>
-                                    <p>${item.price} | {item.cal} Cal</p>
-                                    <p>{item.description}</p>
-                                </div>
-                                <hr />
-                                <Button id='createItem' type='submit' onClick={() => removeItem(item.id)} variant='danger'>Remove Item</Button>
-                            </Card>
-                        </Col>
-                    );
-                })}
-                </Row>
+                    <Row>
+                    {bag.map((itemID) => {
+                        const item = items.find(item => item.id === itemID);
+                        return (
+                            <Col key={item.id} xs={12} md={3}>
+                                <Card className="p-2" style={{ height: '32em' }}>
+                                    <CardImg
+                                        variant='top'
+                                        src={item.image}
+                                        alt={item.name}
+                                        style={{ width: '100%', height: '10em' }}
+                                    />
+                                    <Card.Text>
+                                        <b>{item.name}</b>
+                                    </Card.Text>
+                                    <hr />
+                                    <div className="flex-grow-1" style={{ backgroundImage: 'linear-gradient(105deg, darkred, black)', color: 'white', borderRadius: '15%' }}>
+                                        <p>${item.price} | {item.cal} Cal</p>
+                                        <p>{item.description}</p>
+                                    </div>
+                                    <hr />
+                                    <Button id='createItem' type='submit' onClick={() => removeItem(item.id)} variant='danger'>Remove Item</Button>
+                                </Card>
+                            </Col>
+                        );
+                    })}
+                    </Row>
 
-                <Button 
-                variant="primary" 
-                onClick={() => handleCheckout()} 
-                style={{ width: '100%', maxWidth: '200px', margin: '0 auto' }}
-                    >Place Order</Button>
-            </Card>
+                    <Button 
+                    variant="primary" 
+                    onClick={() => handleCheckout()} 
+                    style={{ width: '100%', maxWidth: '200px', margin: '0 auto' }}
+                        >Place Order</Button>
+                </Card> :
+                <Card style={{ border: '1px solid black', textAlign: 'center' }}>
+                    <Link className='menuLink menuButton' to="/Menu">Menu</Link>
+                </Card>
+
+            }
+            
 
 
         </Container>
