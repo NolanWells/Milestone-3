4db0a477f45bc8537e582781d4e5e5ae5798bff1
Jose Martinez <josemartinez@Joses-Air.lan>
2023-10-31 19:32:50 -0700

    fixed the nav bar issue that combines css position and css flex box. 'there should either be flexbox OR CSS positioning not both'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,62 +12,64 @@ import Register from './views/Register';
 import Checkout from './views/Menu/Checkout';
 
 function App() {
-  return (
-    <Router>
-      <Container className='navBar'>
+    return (
+        <Router>
+            <Container className='navBar'>
 
-        <Nav className='navLinks'>
+                <Nav className='navLinks'>
 
-          <Nav.Item>
-            <Nav.Link href="/">
-              <Link className='homeLink' to="/">Home</Link>
-            </Nav.Link>
-          </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/">
+                            <Link className='homeLink' to="/">Home</Link>
+                        </Nav.Link>
+                    </Nav.Item>
 
-          <Nav.Item>
-            <Nav.Link href="/">
-              <Link className='menuLink' to="/Menu">Menu</Link>
-            </Nav.Link>
-          </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/">
+                            <Link className='menuLink' to="/Menu">Menu</Link>
+                        </Nav.Link>
+                    </Nav.Item>
 
-          <Nav.Item>
-            <Nav.Link href="/">
-              <Link className='loginLink' to="/Login" >Login</Link>
-            </Nav.Link>
-          </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/">
+                            <Link className='loginLink' to="/Login" >Login</Link>
+                        </Nav.Link>
+                    </Nav.Item>
 
-          <Nav.Item>
-            <Nav.Link href="/">
-              <Link className='checkoutLink' to="/Checkout" style={{ textDecoration: 'none' }}></Link>
-            </Nav.Link>
-          </Nav.Item>
+                    {/* <Nav.Item>
+                        <Nav.Link href="/">
+                            <Link className='checkoutLink' to="/Checkout" style={{ textDecoration: 'none' }}></Link>
+                        </Nav.Link>
+                    </Nav.Item> */}
+                </Nav>
 
-          <a href='/'>
-            <img src='https://www.redrobin.com/assets/images/RR_Logo.svg'
-              style={{ width: '200px', height: '200px', position: 'absolute', left: '45%', top: '-4%' }}
-              to="/"
-            /></a>
 
-          <a href='/checkout'>
-            <img src='https://cdn-icons-png.flaticon.com/128/8091/8091819.png'
-              style={{ width: '80px', height: '80px', position: 'relative', left: '1000%', top: '0%' }}
-              to="/"
-            /></a>
+                <a href='/'>
+                    <img src='https://www.redrobin.com/assets/images/RR_Logo.svg'
+                        style={{ width: '125px', height: '100px' }}
+                        to="/"
+                    /></a>
 
-        </Nav>
+                <a href='/checkout' className='basketLogo'>
+                    <img src='https://cdn-icons-png.flaticon.com/128/8091/8091819.png'
+                        style={{ width: '100px', height: '100px' }}
+                        to="/"
+                    />
+                </a>
 
-      </Container>
 
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/Menu" element={<Menu/>}/>
-        {/* <Route path="/Order" element={<Order/>}/> */}
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/Register" element={<Register/>}/>
-        <Route path="/Checkout" element={<Checkout />} />
-      </Routes>
-    </Router >
-  );
+            </Container>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Menu" element={<Menu />} />
+                {/* <Route path="/Order" element={<Order/>}/> */}
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Register" element={<Register />} />
+                <Route path="/Checkout" element={<Checkout />} />
+            </Routes>
+        </Router >
+    );
 }
 
 export default App;
